59b9712e7907ab09eb8915bb1eb8a3ccdc14fcfe
mikolajgawronski <gawronski.miko@gmail.com>
2024-01-24 13:29:24 +0100

    #2 - add currency model and migration

---
+++ nbp/app/Models/Currency.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $id
+ * @property string $code
+ * @property string $name
+ * @property float $rate
+ */
+class Currency extends Model
+{
+    use HasFactory;
+}
